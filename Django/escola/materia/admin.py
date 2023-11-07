c7fe5ceaead9546cf4ac80817bfdd58ae7c4482d
Ø₮áVłØ ฿. Ɽ₳₥ØⱫ <129871291+tavioramoz@users.noreply.github.com>
2023-11-07 14:36:58 -0300

    Delete Django/escola directory

--- Django/escola/materia/admin.py
+++
@@ -1,6 +0,0 @@
-
-from django.contrib import admin
-
-from .models import aluno
-
-admin.site.register(aluno)
--- Django/escola/materia/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class aluno(models.Model):
-    nome = models.CharField(max_length=100)
-    idade = models.IntegerField()
-    cidade = models.CharField(max_length=100)
-
